a9869dc83a2c987d1573c37d580b129388dc4c83
Thien Nguyen <neiht0805@gmail.com>
2023-07-21 22:36:12 -0400

    Fixed decimal button

--- main.js
+++ main.js
@@ -10,23 +10,27 @@ const buttons = document.querySelectorAll('button')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     if (button.parentElement.classList == 'numbers') {
-      temp += button.textContent
+      if (button.textContent == '.' && temp.includes('.')) {
+        temp += ''
+      } else if (button.textContent == '.' && temp == '') {
+        temp = '0.'
+      } else {
+        temp += button.textContent
+      }
+
       displayArea.textContent = temp
-    } 
-    // else if (button.textContent == '=') {
-    //   secondNum = displayArea.textContent
-    //   displayArea.textContent = operate(firstNum, operator, secondNum)
-    //   firstNum = displayArea.textContent
-    // } 
-    else if (button.parentElement.className == 'operations') {
+
+    } else if (button.parentElement.className == 'operations') {
       if (firstNum) {
         secondNum = displayArea.textContent
         displayArea.textContent = operate(firstNum, operator, secondNum)
         firstNum = displayArea.textContent
       }
+      // reset temp but not display so the typed number can be assigned to firstNum
       temp = ''
       firstNum = displayArea.textContent
       operator = button.textContent
+
       if (button.textContent == '=') {
         firstNum = ''
         operator = ''
